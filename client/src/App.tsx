4b5ccf64237b7336e7419e75fdf18a6d3750c799
zhwang2001 <zhwang2001@gmail.com>
2023-06-16 02:42:19 -0400

    changed dimensions of extension

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import {useState, JSX} from 'react'
+import { useState, JSX } from 'react'
 import InputNotes from "./views/inputNotes/inputNotes";
 import FlashCards from "./views/flashCards/flashCards"
-import {LinearProgress} from "@mui/material";
+import { LinearProgress } from "@mui/material";
 import backgroundImage from './assets/background_image.jpg'
 
 function App(): JSX.Element {
@@ -23,16 +23,20 @@ function App(): JSX.Element {
     const [response, setResponse] = useState<string[][]>([]);
     //array that stores the different pages viewed by user
     const pages: Readonly<JSX.Element[]> = [
-        <InputNotes handlePageChange={handlePageChange} setResponse={setResponse}/>,
+        <InputNotes handlePageChange={handlePageChange} setResponse={setResponse} />,
         response.length !== 0
-            ? <FlashCards handlePageChange={handlePageChange} response={response} setResponse={setResponse}/>
-            : <div style={{width: '30vw'}}><LinearProgress sx={{width: '100%'}}/></div>
+            ? <FlashCards handlePageChange={handlePageChange} response={response} setResponse={setResponse} />
+            : <div style={{ width: '30vw' }}><LinearProgress sx={{ width: '100%' }} /></div>
     ]
 
     return (
         <div style={{
+            /*
             width: '700px',
             height: '600px',
+            */
+            width: '100vw',
+            height: '100vh',
             padding: '5% 0% 5% 0%',
             borderRadius: '20px',
             display: 'flex',
--- client/src/views/flashCards/flashCards.tsx
+++ client/src/views/flashCards/flashCards.tsx
@@ -1,12 +1,12 @@
-import React, {JSX} from 'react';
-import {Card, CardContent, Divider, IconButton, Typography} from "@mui/material";
-import {IoCopyOutline, IoPencilOutline, IoTrashOutline} from "react-icons/io5";
-import {IoIosArrowBack} from "react-icons/io";
+import React, { JSX } from 'react';
+import { Card, CardContent, Divider, IconButton, Typography } from "@mui/material";
+import { IoCopyOutline, IoPencilOutline, IoTrashOutline } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
 
 interface FlashCardsProps {
-    handlePageChange: (direction: string) => void;
-    response: string[][];
-    setResponse: React.Dispatch<React.SetStateAction<string[][] | []>>;
+  handlePageChange: (direction: string) => void;
+  response: string[][];
+  setResponse: React.Dispatch<React.SetStateAction<string[][] | []>>;
 }
 /**
  * @brief A functional UI component that displays the quiz name and all the flashcards.
@@ -15,28 +15,28 @@ interface FlashCardsProps {
  * @returns {JSX.Element} The title and an array of flashcards.
  */
 const FlashCards: React.FC<FlashCardsProps> = (props: FlashCardsProps): React.ReactElement => {
-    const {response, handlePageChange, setResponse} = props;
+  const { response, handlePageChange, setResponse } = props;
 
-    return (
-        <CardContent sx={{height: '100vh', padding: '10%'}}>
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-                <div style={{display: 'flex', alignItems: 'center'}}>
-                    <IconButton onClick={() => {
-                        handlePageChange("backward");
-                        setResponse([])
-                    }}>
-                        <IoIosArrowBack size={35}/>
-                    </IconButton>
-                </div>
-                <Typography variant="h2" color="text.primary" sx={{margin: '30px 0px 30px 10px', width: '100%'}}>
-                    Quiz
-                </Typography>
-            </div>
-            {response.map((data: string[], index: number) => (
-                <FlashCard key={index} data={data}/>
-            ))}
-        </CardContent>
-    );
+  return (
+    <CardContent sx={{ height: '100vh', padding: '10%' }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton onClick={() => {
+            handlePageChange("backward");
+            setResponse([])
+          }}>
+            <IoIosArrowBack size={35} />
+          </IconButton>
+        </div>
+        <Typography variant="h2" color="text.primary" sx={{ margin: '30px 0px 30px 10px', width: '100%' }}>
+          Quiz
+        </Typography>
+      </div>
+      {response.map((data: string[], index: number) => (
+        <FlashCard key={index} data={data} />
+      ))}
+    </CardContent>
+  );
 };
 
 
@@ -49,35 +49,35 @@ export default FlashCards
  * @param {object} props the props object
  * @returns {JSX.Element} the question and answer to the flashcard as well as buttons to interact with the flashcard
  */
-function FlashCard (props: { data: Array<string> }) :JSX.Element {
-    return (
-        <CardContent sx={{width: '30vw', display: 'flex', flexFlow: 'row nowrap'}}>
-            <Card sx={{
-                width: '100%',
-                backgroundColor: '#f2f2f2',
-                padding: '20px',
-            }}>
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <Typography variant={"h6"} color={"text.primary"} sx={{fontWeight: '600'}}>
-                        {props.data[0]}
-                    </Typography>
-                </div>
-                <Divider sx={{margin: '10px 0px 10px 0px'}} light/>
-                <Typography variant={"h6"} color={"text.secondary"}>
-                    {props.data[1]}
-                </Typography>
-            </Card>
-            <div style={{
-                display: 'flex',
-                flexFlow: 'column nowrap',
-                justifyContent: 'space-around',
-                padding: '10px',
-            }}>
-                <IconButton><IoCopyOutline/></IconButton>
-                <IconButton><IoPencilOutline/></IconButton>
-                <IconButton><IoTrashOutline/></IconButton>
-            </div>
-        </CardContent>
-    )
+function FlashCard(props: { data: Array<string> }): JSX.Element {
+  return (
+    <CardContent sx={{ width: '30vw', display: 'flex', flexFlow: 'row nowrap' }}>
+      <Card sx={{
+        width: '100%',
+        backgroundColor: '#f2f2f2',
+        padding: '20px',
+      }}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <Typography variant={"h6"} color={"text.primary"} sx={{ fontWeight: '600' }}>
+            {props.data[0]}
+          </Typography>
+        </div>
+        <Divider sx={{ margin: '10px 0px 10px 0px' }} light />
+        <Typography variant={"h6"} color={"text.secondary"}>
+          {props.data[1]}
+        </Typography>
+      </Card>
+      <div style={{
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        justifyContent: 'space-around',
+        padding: '10px',
+      }}>
+        <IconButton><IoCopyOutline /></IconButton>
+        <IconButton><IoPencilOutline /></IconButton>
+        <IconButton><IoTrashOutline /></IconButton>
+      </div>
+    </CardContent>
+  )
 }
 
